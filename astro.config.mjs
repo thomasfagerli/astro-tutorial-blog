eec858aa0d138c04e9f6f6c2df378392f6b500f4
samegener <thomaf0107@yahoo.no>
2023-10-17 14:55:11 -0700

    changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://thomasfagerli.com"
+  site: "https://thomasfagerli.com",
+  integrations: [preact()]
 });
